e26c4d549fd1f4aaca7fad78467ae4a86b9d0740
Marcelo Furtado de Oliveira <marcelofurtado257@gmail.com>
2022-12-18 22:31:41 -0300

    gitignore

---
+++ Jogo da Velha/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+
+public class program
+{
+    static void Main(string[] args)
+    {
+        string[,] jogoDaVelha = new string[3, 3];
+        do
+        {
+            string[] vet = Console.ReadLine().Split();
+            int l = int.Parse(vet[0]);
+            int c = int.Parse(vet[1]);
+            jogoDaVelha[l, c] = "X";
+
+            Console.Write(jogoDaVelha[l, c]);
+            Console.WriteLine("  |  |  ");
+            Console.WriteLine("--|--|--");
+            Console.WriteLine("--|--|--");
+            Console.WriteLine("  |  |  ");
+
+        } while (true);
+
+    }
+
+}
